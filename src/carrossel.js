31bd9038bbce50b4e7e20899816e59ac9ff4cd3c
Rafael-Correzi <112991986+Rafael-Correzi@users.noreply.github.com>
2025-03-26 15:08:29 -0300

    Simplificar carrossel

--- src/carrossel.js
+++ src/carrossel.js
@@ -1,4 +1,3 @@
-const frame = document.querySelector("#frame");
 const imgGrupo = document.querySelector("#img-grupo");
 const imgs = imgGrupo.querySelectorAll(":scope > picture");
 const anterior = document.querySelector("#anterior");
@@ -7,7 +6,7 @@ const divBotoes = document.querySelector("#botoes-carrossel");
 
 //Adiciona botões para trocar a imagem do carrossel
 (function () {
-  for (let i = 1; i < imgs.length -1; i++) {
+  for (let i = 0; i < imgs.length; i++) {
     const botao = document.createElement("button");
     botao.classList.add("botao-carrossel");
     divBotoes.appendChild(botao);
@@ -27,31 +26,10 @@ function closureImg() {
   return atualizarImg;
 }
 
-function estadoTransicao() {
-  let pronto = false;
-
-  function confirmarPronto() {
-    pronto = true;
-    return pronto;
-  }
-
-  function negarPronto() {
-    pronto = false;
-    return pronto;
-  }
-
-  function checarPronto() {
-    return pronto;
-  }
-  
-  return { confirmarPronto, negarPronto, checarPronto };
-}
-
 const atual = closureImg();
-const pronto = estadoTransicao();
 
 function trocarImg(nova) {
-  const transformIntervalo = 100 / imgs.length;
+  const transformIntervalo = 100 / imgs.length ;
   const transform = `${nova * -transformIntervalo}%`;
   //Aplica um transform dinâmico dependendo do número de passos;
   imgGrupo.style.transform = `translateX(${transform})`;
@@ -62,56 +40,52 @@ function colorirBotao(i) {
   botoes.forEach((b) => {
     b.classList.remove("selecionado");
   })
-  botoes[i - 1].classList.add("selecionado");
+  if (i < 0) i = imgs.length - 1;
+  botoes[i].classList.add("selecionado");
 }
 
-function irPraPrimeira() {
-  if (pronto.checarPronto()) {
-  imgGrupo.classList.remove("img-grupo-transition");
-  trocarImg(0);
-  colorirBotao(1);
-  setTimeout(() => {
-    imgGrupo.classList.add("img-grupo-transition");
-    trocarImg(1);
-  }, 1);
+function proximaImg() {
+  if (atual() <= imgs.length - 2) {
+    trocarImg(atual() + 1);
+    colorirBotao(atual());
+  }
+  else {
+    trocarImg(0);
+    colorirBotao(0);
   }
 }
 
-(function () {
-  imgGrupo.addEventListener("transitionend", () => {
-    pronto.confirmarPronto();
-    console.log(pronto.checarPronto());
-  });
-  imgGrupo.addEventListener("transitionstart", () => {
-    pronto.negarPronto();
-    console.log(pronto.checarPronto());
-  })
-})();
+function voltarImg() {
+  if (atual() >= 1) {
+    trocarImg(atual() - 1);
+    colorirBotao(atual());
+  }
+  else {
+    trocarImg(imgs.length - 1);
+    colorirBotao(imgs.length - 1);
+  }
+}
+
 
 (function () {
   proxima.addEventListener("mousedown", () => {
-    if (atual() <= imgs.length - 3) {
-      trocarImg(atual() + 1);
-      colorirBotao(atual());
-    }
-    else irPraPrimeira();
+    imgGrupo.classList.add("img-grupo-transition");
+    proximaImg();
   });
 })();
 
 (function () {
   anterior.addEventListener("mousedown", () => {
-    if (atual() >= 2) {
-      trocarImg(atual() - 1);
-      colorirBotao(atual());
-    }
+    imgGrupo.classList.add("img-grupo-transition");
+    voltarImg();
   });
 })();
 
 (function () {
   for (let i = 0; i < botoes.length; i++) {
     botoes[i].addEventListener("mousedown", () => {
-      trocarImg(i + 1);
-      colorirBotao(i + 1);
+      trocarImg(i);
+      colorirBotao(i);
     });
   }
 })();
@@ -119,14 +93,3 @@ function irPraPrimeira() {
 (function () {
   botoes[0].classList.add("selecionado");
 })();
-
-
-//Adicionar transições à página só depois do primeiro transform para a primeira imagem 
-(function () {
-  imgGrupo.classList.remove("img-grupo-transition");
-  trocarImg(1);
-  setTimeout(() => {
-    imgGrupo.classList.add("img-grupo-transition");
-  }, 10);
-})();
-
